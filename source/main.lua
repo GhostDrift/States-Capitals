d958e0256195ec7c200c01b9e458bfc46ff15553
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-08-21 21:57:59 -0600

    resized the map image

--- source/main.lua
+++ source/main.lua
@@ -8,17 +8,21 @@ import "CoreLibs/nineslice"
 
 local pd <const> = playdate
 local gfx <const> = playdate.graphics
-local backgroudnSprite = nil
+local mapSprite = nil
 
 function initialize()
-    local backgroundImage= gfx.image.new("images/map") --make sure the image is a png, jpgs don't work
-	gfx.sprite.setBackgroundDrawingCallback (
-		function(x,y,width, height)
-			gfx.setClipRect(x,y,width,height)
-			backgroundImage:draw(0,0) -- draws the backgroud image in the upper corner
-			gfx.clearClipRect()
-		end
-	)
+    --local backgroundImage= gfx.image.new("images/map") --make sure the image is a png, jpgs don't work
+	--gfx.sprite.setBackgroundDrawingCallback (
+	--	function(x,y,width, height)
+	--		gfx.setClipRect(x,y,width,height)
+	--		backgroundImage:draw(0,0) -- draws the backgroud image in the upper corner
+	--		gfx.clearClipRect()
+	--	end
+	--)
+	local mapImage = gfx.image.new("images/map")
+	mapSprite = gfx.sprite.new(mapImage)
+	mapSprite:moveTo(200,135)
+	mapSprite:add()
 end
 initialize()
 
